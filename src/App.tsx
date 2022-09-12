fea0f816881054633f1a5f36e0e36519668693bd
Pavel <designdpcompany@gmail.com>
2022-09-12 17:39:46 +0300

    01-09 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div>
+     <Header/>
+        <Technologies/>
     </div>
   );
 }
