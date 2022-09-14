c913296b5eef48f47f69faa89ed3d0edb266729a
Pavel <designdpcompany@gmail.com>
2022-09-14 22:52:06 +0300

    Navbar, Header, Profile comp created

---
+++ src/components/Profile.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+const Profile = () => {
+    return (
+        <main className='content'>
+            <div>
+                <img
+                    src="https://images.pexels.com/photos/167699/pexels-photo-167699.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    alt=""/>
+            </div>
+            <div>
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw9cnEvZ98kadKb7ZIhW20sXOTNVlEsH1q4Q&usqp=CAU"
+                    alt=""/>
+            </div>
+        </main>
+    );
+}
+
+export default Profile;
